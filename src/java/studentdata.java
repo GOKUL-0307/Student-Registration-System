96eed017451cf5211bb29babf3a234bfc5a13063
GOKUL M.P <119731944+GOKUL-0307@users.noreply.github.com>
2023-12-16 19:04:47 +0530

    Update studentdata.java

--- src/java/studentdata.java
+++ src/java/studentdata.java
@@ -1,103 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.sql.*;
 
-/**
- *
- * @author admin
- */
 public class studentdata extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out=response.getWriter();
-             Connection con=null;
-             Statement stmt=null;
-             String s1=request.getParameter("Name");
-            String s2=request.getParameter("Regno");
-            String s3=request.getParameter("Dept");
-            String s4=request.getParameter("Year");
-             String s5=request.getParameter("username");
-            String s6=request.getParameter("password");
-            
-             
-            
-            try
-        {
+        PrintWriter out = response.getWriter();
+        Connection con = null;
+        Statement stmt = null;
+        String s1 = request.getParameter("Name");
+        String s2 = request.getParameter("Regno");
+        String s3 = request.getParameter("Dept");
+        String s4 = request.getParameter("Year");
+        String s5 = request.getParameter("username");
+        String s6 = request.getParameter("password");
+
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost/user","root","gokul");
-            stmt=con.createStatement();
-            stmt.executeUpdate("insert into student values('"+s1+"','"+s2+"','"+s3+"','"+s4+"','"+s5+"','"+s6+"')");
-            out.println("<br>");
-            out.println("<br>");
-            out.println("<br>");
-            out.println("<br>");
-            out.println("<h1 align=center style=color:deepskyblue>Registered Successfully</h1>");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/user", "root", "gokul");
+            stmt = con.createStatement();
+
+            // Check if the registration number already exists
+            if (isRegistrationNumberExists(stmt, s2)) {
+                // If registration number exists, display an error message
+                out.println("<h1 align=center style=color:red;font-weight:bold>Data already exists!</h1>");
+            } else {
+                // If registration number doesn't exist, insert the data into the database
+                stmt.executeUpdate("insert into student values('" + s1 + "','" + s2 + "','" + s3 + "','" + s4 + "','" + s5 + "','" + s6 + "')");
+                out.println("<br>");
+                out.println("<br>");
+                out.println("<br>");
+                out.println("<br>");
+                out.println("<h1 align=center style=color:deepskyblue;font-weight:bold>Registered Successfully</h1>");
+            }
+
+        } catch (Exception e) {
+            out.println("Sorry!!! Try again later.");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (con != null) con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        catch(Exception e){
-            out.println("sorry!!! Try again later.");
-        }
-            out.println("<head>");
-            out.println("<style> body{ background-image: url('c2.jpg');background-size: 95% 100%;}</style>");
-            out.println("</head>");
+        out.println("<head>");
+        out.println("<style> body { background-color: rgb(255, 208, 252); }</style>");
+
+        out.println("</head>");
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private boolean isRegistrationNumberExists(Statement stmt, String regNo) throws Exception {
+        String query = "SELECT * FROM student WHERE Reg_No='" + regNo + "'";
+        return stmt.executeQuery(query).next();
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         doGet(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
